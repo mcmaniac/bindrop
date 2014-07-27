293ecc94ec157b03d0204b0df6eeffa68a372240
kvitebjorn <kvitebjorn.k@gmail.com>
2014-07-26 21:12:24 -0600

    added file upload form

--- src/HTML/Index.hs
+++ src/HTML/Index.hs
@@ -3,11 +3,15 @@
 module HTML.Index where
 
 import Text.Blaze.Html
-import Text.Blaze.Html5
+import Text.Blaze.Html5 as H
+import Text.Blaze.Html5.Attributes as A
 
 import HTML.Base
 
 index :: Html
 index = baseHtml $ do
-  title "asd"
-  h1 "asd"
+  H.title "asd"
+  H.h1 "asd"
+  H.p "Select a file: "
+  H.form $ do H.input ! A.type_  "file" ! A.name  "fileUpload"
+
