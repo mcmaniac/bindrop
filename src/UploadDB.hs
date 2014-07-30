0ff78a137f592fe2e2a42be481509ab0140453c3
kvitebjorn <kvitebjorn.k@gmail.com>
2014-07-29 23:43:37 -0600

    initial acid-state and ixset

---
+++ src/UploadDB.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE DeriveDataTypeable, GeneralizedNewtypeDeriving,
+  TemplateHaskell, TypeFamilies, RecordWildCards,
+  OverloadedStrings #-}
+
+module UploadDB where
+
+import Control.Applicative  ( (<$>) )
+import Data.Acid            ( AcidState, Query, Update
+                            , makeAcidic, openLocalState )
+import Data.Acid.Advanced   ( query', update' )
+import Data.Acid.Local      ( createCheckpointAndClose )
+import Data.Data            ( Data, Typeable )
+import Data.IxSet           ( Indexable(..), IxSet(..), (@=)
+                            , Proxy(..), getOne, ixFun, ixSet )
+import qualified Data.IxSet as IxSet
+import Data.SafeCopy        ( SafeCopy, base, deriveSafeCopy )
+import Data.Text            ( Text )
+import Data.Text.Lazy       ( toStrict )
+import qualified Data.Text as Text
+import Data.Time            ( UTCTime(..), getCurrentTime )
+
+data FileUpload = FileUpload { path    :: FilePath
+                             , name    :: String
+                             } deriving (Eq, Ord, Show, Data, Typeable)
+
+$(deriveSafeCopy 0 'base ''FileUpload)
+
+--data UploadDB = UploadDB { uploads :: IntMap FileUpload }
+--  deriving (Typeable)
+
