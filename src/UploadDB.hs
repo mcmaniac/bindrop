a7b2bf4b2672c0d32dcfc983282219960673499a
kvitebjorn <kvitebjorn.k@gmail.com>
2014-07-29 23:54:47 -0600

    added newtypes for indexing

--- src/UploadDB.hs
+++ src/UploadDB.hs
@@ -19,12 +19,15 @@ import Data.Text.Lazy       ( toStrict )
 import qualified Data.Text as Text
 import Data.Time            ( UTCTime(..), getCurrentTime )
 
-data FileUpload = FileUpload { path    :: FilePath
-                             , name    :: String
+data FileUpload = FileUpload { path :: FilePath
+                             , name :: String
                              } deriving (Eq, Ord, Show, Data, Typeable)
 
 $(deriveSafeCopy 0 'base ''FileUpload)
 
---data UploadDB = UploadDB { uploads :: IntMap FileUpload }
---  deriving (Typeable)
+newtype UploadPath = UploadPath FilePath
+  deriving (Eq, Ord, Data, Typeable, SafeCopy)
+
+newtype UploadName = UploadName String
+  deriving (Eq, Ord, Data, Typeable, SafeCopy)
 
