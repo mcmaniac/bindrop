adc81786db2fcfd19d5a06d4654d2153071791e7
kvitebjorn <kvitebjorn.k@gmail.com>
2014-08-18 22:02:54 -0600

    changed recent upload list styling

--- src/HTML/File.hs
+++ src/HTML/File.hs
@@ -16,9 +16,9 @@ recentFile file = toHtml $ do
   let dlLink   = "/s/" ++ file ^. sfname
   let infoLink = "localhost:8082/f/" ++ file ^. sfname
 
-  H.p $ do
-    a ! href (toValue dlLink) $ H.toHtml fileName
-  H.p (H.toHtml $ show fileTime)
-  H.p (H.toHtml $ infoLink)
-  H.br
+  H.div ! A.id "recentInfo" $ do
+    H.p $ do
+      a ! href (toValue dlLink) $ H.toHtml fileName
+    H.p (H.toHtml $ show fileTime)
+    H.p (H.toHtml $ infoLink)
 
