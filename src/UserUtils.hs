3665f97b91de27114e6a1b2a2847a26e32af5809
kvitebjorn <kvitebjorn.k@gmail.com>
2014-08-19 18:51:32 -0600

    quick fix

--- src/UserUtils.hs
+++ src/UserUtils.hs
@@ -3,6 +3,11 @@ module UserUtils where
 import Crypto.Scrypt
 import qualified Data.ByteString as B
 
-mkEncrypted :: ByteString -> IO EncryptedPass
+mkEncrypted :: B.ByteString -> IO EncryptedPass
 mkEncrypted pw = encryptPassIO defaultParams (Pass pw)
+
+{- todo
+uRegisterPart :: AcidState Users -> user? -> ServerPart Response
+uRegisterPart uAcid user =
+-}
 
